060bcffc99085b223f87e602893685354e5e0ff2
mmbower1 <mttbwr91@gmail.com>
2025-11-14 10:44:12 -0800

    transfer scss to cart dropdown component

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -17,8 +17,31 @@ const CartDropdown = () => {
 
   return (
     <div>
-      <div className="cart-dropdown-container">
-        <div className="cart-items">
+      <div
+        className="cart-dropdown-container"
+        style={{
+          position: "absolute",
+          width: "320px",
+          height: "400px",
+          display: "flex",
+          flexDirection: "column",
+          padding: "20px",
+          border: "1px solid black",
+          backgroundColor: "white",
+          top: "160px",
+          right: "80px",
+          zIndex: 5,
+        }}
+      >
+        <div
+          className="cart-items"
+          style={{
+            height: "320px",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+          }}
+        >
           {cartItems.map((item) => (
             <CartItem key={item.id} cartItem={item} />
           ))}
